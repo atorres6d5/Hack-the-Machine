4ac3cd7097efc642949bed60a30f821944f346d2
Aldo Torres <atorres.dev@gmail.com>
2018-09-22 14:07:29 -0700

    edit

--- src/reducers/homeReducer.js
+++ src/reducers/homeReducer.js
@@ -1,5 +1,5 @@
 import {
-
+RETURN_HOME
 } from '../actions/types.js'
 
 const INITIAL_STATE = {
@@ -11,7 +11,7 @@ export default ( state = INITIAL_STATE, action)=>{
   console.log(action)
 
   switch (action.type) {
-    case EMAIL_TEXT_CHANGE:
+    case RETURN_HOME:
       return{ ...state, email:action.payload }
 
     default:
