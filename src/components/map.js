9dfa61ea4ec3886f73e5553d78c0113c3db59a14
Clarisa Leu-Rodriguez <clarisarodriguezaz@gmail.com>
2018-09-23 10:26:48 -0700

    Added menu item to all pgs.

---
+++ src/components/map.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import RespondersTable from './RespondersTable';
+import { getResponders } from '../actions/index';
+import { Menu } from 'semantic-ui-react';
+
+export default class map extends Component {
+  state = { activeItem: 'Live Map' };
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+  render() {
+    const { activeItem } = this.state;
+    return (
+      <div>
+        {/* // Menu */}
+        <Menu fluid widths={6} size="massive">
+          <Menu.Item
+            name="Home"
+            active={activeItem === 'Home'}
+            onClick={() => this.props.history.push('/')}
+          />
+
+          <Menu.Item
+            name="Responders"
+            active={activeItem === 'Responders'}
+            onClick={() => this.props.history.push('/Responders')}
+          />
+
+          <Menu.Item
+            name="Dispatch Services"
+            active={activeItem === 'Dispatch Services'}
+            onClick={() => this.props.history.push('/Dispatch')}
+          />
+
+          <Menu.Item
+            name="Live Map"
+            active={activeItem === 'Live Map'}
+            onClick={() => this.props.history.push('/Map')}
+          />
+
+          <Menu.Item
+            name="About"
+            active={activeItem === 'About'}
+            onClick={() => this.props.history.push('/About')}
+          />
+
+          <Menu.Item
+            name="Help"
+            active={activeItem === 'Help'}
+            onClick={() => this.props.history.push('/Help')}
+          />
+        </Menu>
+      </div>
+    );
+  }
+}
