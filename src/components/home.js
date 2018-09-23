43ddf3d1cb91b3d4d88e5d1870da06d8fb1325d8
Aldo Torres <atorres.dev@gmail.com>
2018-09-23 01:59:21 -0700

    history

--- src/components/home.js
+++ src/components/home.js
@@ -12,9 +12,9 @@ class Home extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <a className="App-intro" onClick={()=>this.props.history.push('/Responders')}>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </a>
       </div>
     );
   }
