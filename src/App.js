5d2b4eafb4fd2831f0267f27163f8ebc800f8b65
Jerry Aldrich <jerryaldrichiii@gmail.com>
2018-09-22 23:44:50 -0700

    Test autodeploy
    Signed-off-by: Jerry Aldrich <jerryaldrichiii@gmail.com>

--- src/App.js
+++ src/App.js
@@ -8,11 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Jerry&#39;s secret React, do not read!</h1>
+          <h1 className="App-title">Super secret page...do not read</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
