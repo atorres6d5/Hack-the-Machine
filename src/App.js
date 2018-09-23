6229c07009d22e5ffbb9d3f46981906a4d579ce1
Aldo Torres <atorres.dev@gmail.com>
2018-09-23 08:22:56 -0700

    fix merge

--- src/App.js
+++ src/App.js
@@ -7,13 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-<<<<<<< HEAD
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">Welcome to React</h1>
-=======
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Super secret page...do not read</h1>
->>>>>>> 5d2b4eafb4fd2831f0267f27163f8ebc800f8b65
         </header>
       </div>
     );
