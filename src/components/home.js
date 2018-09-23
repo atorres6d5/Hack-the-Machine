db0bd8875b7ff6943a6dfad56e616bc8b634ab59
Aldo Torres <atorres.dev@gmail.com>
2018-09-23 08:24:54 -0700

    edit

--- src/components/home.js
+++ src/components/home.js
@@ -13,7 +13,7 @@ class Home extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <a className="App-intro" onClick={()=>this.props.history.push('/Responders')}>
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <h1 className="App-title">Super secret page...do not read</h1>
         </a>
       </div>
     );
